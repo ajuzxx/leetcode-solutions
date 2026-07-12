5cc3ddf01bd5fde43b5c6caa716fd296cb8baba5
ajuzxx <ajasmathew2003@gmail.com>
2026-07-12 11:36:19 +0530

    Add binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def search(self, nums, target):
+        
+        left = 0
+        right = len(nums)
+
+        while left< right:
+            mid = (left+right)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                left = mid+1
+            else :
+                right = mid
+        return -1
