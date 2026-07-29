7e9979d7cba08dbf6e1b1962e2d086f8a52ae043
ajuzxx <ajasmathew2003@gmail.com>
2026-07-29 22:26:27 +0530

    feat: add solutions for reverse string and 3sum problems to leetcode directory

---
+++ 3sum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def sortColors(self, nums):
+        left = 0
+        right = len(nums) - 1
+        current =0
+
+        while current<= right:
+            if nums[current]==0:
+
+                nums[left],nums[current] = nums[current],nums[left]
+                current+=1
+                left+=1
+            elif nums[current]==2:
+                nums[right],nums[current] = nums[current],nums[right]
+                right-=1
+            else :
+                current+=1
+        return nums
+
+        
+        
+
+        
